65f9df3f42fddcaf6158b9beecf8b0f5444b3e51
Arth <arthagrawal72@gmail.com>
2025-08-18 19:32:55 +0530

    deploy fixes 3

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,15 @@ app.use(cors({
   origin: ["http://localhost:5173","https://resumebuilder-frontend-8vou.onrender.com"],
   credentials: true, // Allow cookies to be sent
 }));
+
+// Extra headers to support Set-Cookie in cross-site requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
