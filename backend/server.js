c3011799737293737ef44d52797d18d46541e6de
Arth <arthagrawal72@gmail.com>
2025-08-18 18:43:19 +0530

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://resumebuilder-frontend-8vou.onrender.com"],
   credentials: true, // Allow cookies to be sent
 }));
 app.use(express.json());
